05b648fba55ef12495375fcab8afa09d0c11cc1f
milydatta <dmily270@gmail.com>
2022-09-26 13:11:39 +0600

    some update

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Layout.scss';
+
+
+const Layout = () => {
+    return(
+        <div>
+           <h1>This is Layout</h1>
+        </div>
+    );
+};
+
+export default Layout;
